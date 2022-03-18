6c5b9205ee7b13593259c6696bd3aeeffce1c8f7
Douglas Joziel Bitencourt Freitas <douglas.joziel@outlook.com>
2022-03-18 13:43:27 -0300

    Atualizando arquivos do site
    Colocando no GitHub os arquivos do novo layout do site

---
+++ index.js
@@ -0,0 +1,3 @@
+function comprarAgora() {
+   prompt('Olá, seja bem-vind@!\n\nAs vendas foram intensas nas últimas horas. No momento, o produto está indisponível. Deixe seu e-mail aqui que vamos lhe avisar assim que o produto estiver disponíveo outra vez.\n');
+}
