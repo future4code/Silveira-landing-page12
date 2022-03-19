3ab1c94660d3e12a14b716f94885f1735686e76a
FabioMatheus777 <fabioMatheusXLR8@gmail.com>
2022-03-19 01:42:04 -0300

    adicionado seçao com itens destaque, slide e content

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function comprarAgora() {
    prompt('Olá, seja bem-vind@!\n\nAs vendas foram intensas nas últimas horas. No momento, o produto está indisponível. Deixe seu e-mail aqui que vamos lhe avisar assim que o produto estiver disponíveo outra vez.\n');
 }
+
+
+/* Slide reprodution*/
+$('.carousel').carousel({
+   interval: 2000
+ })
